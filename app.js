e07184fe0201d0005dd55049d5de65684951cb8d
huydolo <142735354+huydolo@users.noreply.github.com>
2024-08-14 18:42:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
-// Array of compliments
 const compliments = [
-    "You're an awesome person!",
-    "You have a great sense of humor!",
-    "Your creativity is amazing!",
-    "You're a fantastic problem solver!",
-    "You make the world a better place!",
-    "You have a heart of gold!",
-    "Your positivity is infectious!",
-    "You're a true friend!",
-    "You are incredibly talented!",
-    "You light up the room!",
-    "You are my sunshine",
-    "You make me happy!",
-    "You are the G.O.A.T",
+            "You have a great sense of humor!",
+            "You're an awesome friend.",
+            "You light up the room.",
+            "You're a smart cookie!",
+            "You are amazing just the way you are.",
+            "You bring out the best in others.",
+            "You're a great listener.",
+            "You have impeccable manners.",
+            "You're so thoughtful.",
+            "You're making a difference."
+        ];
 
-];
-
-// Function to get a random compliment
-function getRandomCompliment() {
-    const randomIndex = Math.floor(Math.random() * compliments.length);
-    return compliments[randomIndex];
-}
-
-// Event listener for the button
-document.getElementById('compliment-button').addEventListener('click', function() {
-    const compliment = getRandomCompliment();
-    document.getElementById('compliment').textContent = compliment;
-});
+        document.getElementById('complimentButton').addEventListener('click', function() {
+            const randomIndex = Math.floor(Math.random() * compliments.length);
+            document.getElementById('complimentText').innerText = compliments[randomIndex];
+        });
